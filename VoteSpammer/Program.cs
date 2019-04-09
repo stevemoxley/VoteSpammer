c9708558da8220f3716f45d99e0e96317ee406ae
stevemoxley <sdmfyc@gmail.com>
2019-04-09 16:48:41 -0500

    Proxy voting

--- VoteSpammer/Program.cs
+++ VoteSpammer/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VoteSpammer
@@ -14,90 +15,150 @@ namespace VoteSpammer
         static void Main(string[] args)
         {
 
+            System.Net.ServicePointManager.DefaultConnectionLimit = 20;
+
             Console.WriteLine("10Best Vote Spammer");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Loading Proxy List");
+            ProxyProvider.LoadProxyList();
             Console.WriteLine("---------------------");
             Console.WriteLine("How many votes would you like to send?");
             var numberOfVotes = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < numberOfVotes; i++)
-            {
-                var webInfo = GetWebInfo();
+            Console.WriteLine("Starting voting");
+            StartVoting(numberOfVotes);
 
-                var result = Vote(webInfo);
+            Console.ReadLine();
 
-                Console.WriteLine($"{i+1}: {result}");
-
-            }
         }
 
-
-        private static WebInfo GetWebInfo()
+        static async void StartVoting(int numberOfVotes)
         {
-            var url = "http://www.10best.com/awards/travel/best-zoo/saint-louis-zoo-st-louis/";
+            var allProxies = ProxyProvider.AllWorkingProxies;
 
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.CookieContainer = new CookieContainer();
-
-            using (var response = (HttpWebResponse)request.GetResponse())
+            foreach (var webProxy in allProxies)
             {
-                using (var reader = new StreamReader(response.GetResponseStream()))
+                Console.WriteLine($"Trying vote on proxy { webProxy.Address }");
+                var result = await Vote(webProxy);
+                if(result)
                 {
-                    var document = new HtmlDocument();
-                    document.LoadHtml(reader.ReadToEnd());
-
-                    var voteKey = document.GetElementbyId("votekey").Attributes["value"].Value;
-                    var voteKeyEncoded = WebUtility.UrlEncode(voteKey);
-                    var cookieValue = response.Cookies[1].Value;
-                    var uniqueId = response.Cookies[0].Value;
-
-                    var webInfo = new WebInfo()
-                    {
-                        CookieNumber = cookieValue,
-                        VoteKey = voteKeyEncoded,
-                        UniqueID = uniqueId
-                    };
-
-                    return webInfo;
+                    Console.WriteLine($"Proxy worked. Beginning vote spam");
+                    VoteUsingWorkingProxy(numberOfVotes, webProxy);
                 }
             }
 
+            ProxyProvider.SaveBadProxies();
         }
 
-        private static string Vote(WebInfo webInfo)
+        static void VoteUsingWorkingProxy(int numberOfVotes, WebProxy webProxy)
         {
-            var url = $"http://www.10best.com/common/ajax/vote.php?voteKey={ webInfo.VoteKey }&c={webInfo.CookieNumber }";
+            List<Task<bool>> allTasks = new List<Task<bool>>();
+
+            for (int i = 0; i < numberOfVotes; i++)
+            {
+                allTasks.Add(Vote(webProxy));
+            }
+
+            Task.WaitAll(allTasks.ToArray());
+
+            Console.WriteLine($"Finished voting with proxy { webProxy.Address }");
+        }
+
+
+        private static async Task<WebInfo> GetWebInfo(WebProxy proxy)
+        {
+            var url = "https://www.10best.com/awards/travel/best-zoo-2019/saint-louis-zoo-st-louis/";
 
             var request = (HttpWebRequest)WebRequest.Create(url);
             request.CookieContainer = new CookieContainer();
+            request.Proxy = proxy;
+            request.Timeout = _timeout;
+            request.ReadWriteTimeout = _timeout;
 
-            request.CookieContainer.Add(new Cookie
+            using (var response = await request.GetResponseAsync())
+            using (var reader = new StreamReader(response.GetResponseStream()))
             {
-                Value = webInfo.CookieNumber,
-                Domain = ".www.10best.com",
-                Path = "/",
-                Expires = DateTime.Now + new TimeSpan(365, 0, 0, 0, 0),
-                Name = "rnd",
-            });
+                var httpResponse = (HttpWebResponse)response;
+                var document = new HtmlDocument();
+                document.LoadHtml(reader.ReadToEnd());
 
-            request.CookieContainer.Add(new Cookie
-            {
-                Value = webInfo.UniqueID,
-                Domain = ".www.10best.com",
-                Path = "/",
-                Expires = DateTime.Now + new TimeSpan(365, 0, 0, 0, 0),
-                Name = "uniqID"
-            });
+                var voteKey = document.GetElementbyId("votekey").Attributes["value"].Value;
+                var voteKeyEncoded = WebUtility.UrlEncode(voteKey);
+                var cookieValue = httpResponse.Cookies[1].Value;
+                var uniqueId = httpResponse.Cookies[0].Value;
 
-            using (var response = (HttpWebResponse)request.GetResponse())
+                var webInfo = new WebInfo()
+                {
+                    CookieNumber = cookieValue,
+                    VoteKey = voteKeyEncoded,
+                    UniqueID = uniqueId
+                };
+
+                return webInfo;
+            }
+        }
+
+        private static async Task<bool> Vote(WebProxy proxy)
+        {
+            try
             {
+                var webInfo = await GetWebInfo(proxy);
+
+                var url = $"http://www.10best.com/common/ajax/vote.php?voteKey={ webInfo.VoteKey }&c={webInfo.CookieNumber }";
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.CookieContainer = new CookieContainer();
+                request.Proxy = proxy;
+                request.Timeout = _timeout;
+                request.ReadWriteTimeout = _timeout;
+
+                request.CookieContainer.Add(new Cookie
+                {
+                    Value = webInfo.CookieNumber,
+                    Domain = ".www.10best.com",
+                    Path = "/",
+                    Expires = DateTime.Now + new TimeSpan(365, 0, 0, 0, 0),
+                    Name = "rnd",
+                });
+
+                request.CookieContainer.Add(new Cookie
+                {
+                    Value = webInfo.UniqueID,
+                    Domain = ".www.10best.com",
+                    Path = "/",
+                    Expires = DateTime.Now + new TimeSpan(365, 0, 0, 0, 0),
+                    Name = "uniqID"
+                });
+
+                using (var response = await request.GetResponseAsync())
                 using (var reader = new StreamReader(response.GetResponseStream()))
                 {
                     var result = reader.ReadToEnd();
-                    return result;
+                    if (result.Contains("Voting is not permitted in your location"))
+                    {
+                        ProxyProvider.AddBadProxy(proxy);
+                        return false;
+                    }
+                    Console.WriteLine(result);
+                    return true;
                 }
             }
+            catch (WebException webEx)
+            {
+                Console.WriteLine($"WebException: {webEx.Message}.");
+                ProxyProvider.AddBadProxy(proxy);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                ProxyProvider.AddBadProxy(proxy);
+                return false;
+            }
+
         }
 
+        private static int _timeout = 2000;
 
         class WebInfo
         {
---
+++ VoteSpammer/ProxyProvider.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteSpammer
+{
+    public static class ProxyProvider
+    {
+
+        public static void LoadProxyList()
+        {
+            var proxyLines = File.ReadAllLines("proxyList.txt");
+
+            _badProxies = JsonConvert.DeserializeObject<List<WebProxy>>(File.ReadAllText("badProxies.txt"));
+
+            if(_badProxies == null)
+            {
+                _badProxies = new List<WebProxy>();
+            }
+
+            foreach (var proxy in proxyLines)
+            {
+                var webProxy = new WebProxy(proxy);
+
+                if (_badProxies.Select(p => p.Address).Contains(webProxy.Address))
+                    continue;
+
+                _proxyList.Add(webProxy);
+            }
+        }
+
+        public static List<WebProxy> AllProxies
+        {
+            get
+            {
+                return _proxyList;
+            }
+        }
+
+        public static List<WebProxy> AllWorkingProxies
+        {
+            get
+            {
+                return _proxyList.Where(p => !_badProxies.Contains(p)).ToList();
+            }
+        }
+
+        public static void AddBadProxy(WebProxy proxy)
+        {
+            _badProxies.Add(proxy);
+            SaveBadProxies();
+        }
+
+        public static void SaveBadProxies()
+        {
+            var json = JsonConvert.SerializeObject(_badProxies);
+            File.WriteAllText("badProxies.txt", json);
+        }
+
+        public static bool IsBadProxy(WebProxy proxy)
+        {
+            return _badProxies.Contains(proxy);
+        }
+
+        private static List<WebProxy> _badProxies = new List<WebProxy>();
+        private static List<WebProxy> _proxyList = new List<WebProxy>();
+
+    }
+
+}
